fe1a869434ac2159f16a672559d6085e9617a855
jrbreeden <jrbreeden55@gmail.com>
2022-06-13 12:06:42 -0400

    added rout and database connection

--- server.js
+++ server.js
@@ -44,4 +44,28 @@ const reviewSchema = new Schema({
 // make review model
 const Review = model("Review", reviewSchema);
 
+/////////////////////////////////////////////////
+// Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
 
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")); // logging
+app.use(methodOverride("_method")); //override for put and delete request from forms
+app.use(express.urlencoded({ extended: true})); // parse urlencoded request bodies
+app.use(express.static("public")); // serve files from public statically
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+app.get("/", (req, res) => {
+    res.send("your server is running.");
+  });
+  
+  ///////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
