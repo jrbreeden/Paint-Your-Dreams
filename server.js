3c1cf9b3075bf9569be1087c07cecb74179e9a01
jrbreeden <jrbreeden55@gmail.com>
2022-06-13 15:32:12 -0400

    fixed connections

--- server.js
+++ server.js
@@ -8,24 +8,6 @@ const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const path = require("path")
 
-/////////////////////////////////////////////
-// Database Connection
-/////////////////////////////////////////////
-// Setup inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL;
-const CONFIG = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-// Establish Connection
-mongoose.connect(DATABASE_URL, CONFIG);
-
-// Events for when connection opens/disconnects/errors
-mongoose.connection
-  .on("open", () => console.log("Connected to Mongoose"))
-  .on("close", () => console.log("Disconnected from Mongoose"))
-  .on("error", (error) => console.log(error));
 
   ////////////////////////////////////////////////
 // Our Models
@@ -54,7 +36,7 @@ const app = require("liquid-express-views")(express(), {root: [path.resolve(__di
 /////////////////////////////////////////////////////
 app.use(morgan("tiny")); // logging
 app.use(methodOverride("_method")); //override for put and delete request from forms
-app.use(express.urlencoded({ extended: true})); // parse urlencoded request bodies
+app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
 app.use(express.static("public")); // serve files from public statically
 
 ////////////////////////////////////////////
