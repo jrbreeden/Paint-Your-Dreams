a51db679de8057cbef93b43ef10388c038457823
jrbreeden <jrbreeden55@gmail.com>
2022-06-15 09:52:43 -0400

    fixed login

--- controllers/users.js
+++ controllers/users.js
@@ -58,8 +58,8 @@ router.post("/login", async (req, res) => {
                     // store some properties in the session object
                     req.session.username = username
                     req.session.loggedIn = true
-                    // redirect to fruits page if successful
-                    res.redirect("/fruits");
+                    // redirect to reviews page if successful
+                    res.redirect("/reviews");
                 } else {
                     // error if password doesn't match
                     res.json({ error: "password doesn't match" });
